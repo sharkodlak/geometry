895522ada428a8981a31e513e5313d50b612fbaf
Pavel Stetina <github@shark.kom.cz>
2018-02-23 16:37:12 +0100

    Create Round to easify radians  calculations

---
+++ src/Round.java
@@ -0,0 +1,7 @@
+package sharkodlak.geometry;
+
+public interface Round {
+	public static final double HALF_ROUND = Math.PI;
+	public static final double ROUND = 2 * HALF_ROUND;
+	public static final double PERPENDICULAR = HALF_ROUND / 2;
+}
